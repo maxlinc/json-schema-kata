e4ba7910d0e18a7d26a502e29c45637d4c0f94ef
Max Lincoln <max@devopsy.com>
2014-02-13 21:26:35 -0500

    Use travis-build to run suite

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 require 'json'
 require 'json-schema'
 require 'json_spec'
+require 'fileutils'
 
 RSpec.configure do |c|
   c.filter_run_excluding :schema_version => ENV['EXCLUDE_SCHEMA_VERSION']
@@ -24,14 +25,32 @@ def compare_without_whitespace actual, expected
   expect(actual_cleaned).to eq expected_cleaned
 end
 
+def save_environment_variable key, value
+  "export #{key}=#{value}"
+end
+
+def setup_env_vars vars
+  FileUtils.mkdir_p 'tmp'
+  file = File.open("tmp/challenge_opts", 'w')
+  vars.each do |key, value|
+    file.puts save_environment_variable(key, value)
+  end
+  file.close
+  file.path
+end
+
 def invoke_challenge challenge, example_file
   example_file = File.absolute_path example_file
   Bundler.with_clean_env do
     Dir.chdir '..' do
       example_file = example_file.gsub(Dir.pwd + '/', '')
-      challenge_script = "scripts/challenges/#{challenge}"
-      pending "#{challenge_script} does not exist" unless File.readable? challenge_script
-      `#{challenge_script} #{example_file}`
+      setup_env_vars({
+        'CHALLENGE' => challenge,
+        'SAMPLE_FILE' => example_file
+      })
+      # pending "#{challenge_script} does not exist" unless File.readable? challenge_script
+      system "travis run after_script"
+      File.read "tmp/test_output"
     end
   end
 end
